96ae58c149fef8a1563c81092cbf9bd76dfd98e2
vokur13 <vokur13@gmail.com>
2022-07-17 15:44:26 +0300

    outlay added

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,16 @@
-(() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-  const modalBtnRef = document.querySelector('[data-mdl-btn]');
+;(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]')
+  const mobileMenuRef = document.querySelector('[data-menu]')
+  const modalBtnRef = document.querySelector('[data-mdl-btn]')
 
   menuBtnRef.addEventListener('click', () => {
-    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false
 
-    menuBtnRef.classList.toggle('is-open');
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    menuBtnRef.classList.toggle('is-open')
+    menuBtnRef.setAttribute('aria-expanded', !expanded)
 
-    mobileMenuRef.classList.toggle('is-open');
-    modalBtnRef.classList.toggle('is-open');
-  });
-})();
+    mobileMenuRef.classList.toggle('is-open')
+    modalBtnRef.classList.toggle('is-open')
+  })
+})()
